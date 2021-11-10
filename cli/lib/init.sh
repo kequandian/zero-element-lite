4ccee61916aced7a24507e54a426222a124b0f3f
AEdgejay <471021444@qq.com>
2021-11-10 15:31:21 +0800

    更改依赖库地址

--- cli/lib/init.sh
+++ cli/lib/init.sh
@@ -2,7 +2,7 @@ rm -rf ./pdf-cli
 # 初始化命令
 echo "开始初始化"
 echo "开始下载工具"
-git clone git@gitee.com:aedge/AE_PdfTransform.git --single-branch pdf-cli
+git clone git@github.com:kequandian/zero-element-lite.git --branch pdfTransform --single-branch pdf-cli
 cd pdf-cli
 echo "准备安装"
 npm i
